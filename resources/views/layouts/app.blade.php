ede64053b58e435ca48a64a34e4eac9023aa2857
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-06-21 16:24:19 +0530

    front end for roller gate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @stack('styles')
 </head>
 <body>
     <div id="app">
@@ -76,5 +78,7 @@
             @yield('content')
         </main>
     </div>
+
+@stack('scripts')
 </body>
 </html>

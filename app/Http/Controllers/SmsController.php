30dd5913645d3bb33f0bfc114b950651420b5045
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-06-28 16:41:42 +0530

    Auth mechanism changed

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -14,17 +14,17 @@ class SmsController extends Controller
 
         $sms = new Sms();
         $sms->serial_number = $data[0];
-        $sms->imei = $data[1];
-        $sms->event_name = $data[2];
-        $sms->session_id = $data[3];
-        $sms->sim_ccid = $data[4];
-        $sms->network_operator = $data[5];
-        $sms->signal_quality = $data[6];
-        $sms->battery_voltage = $data[7];
-        $sms->firmware_version = $data[8];
-        $sms->modem_fail = $data[9];
-        $sms->network_fail = $data[10];
-        $sms->gprs_fail = $data[11];
+        $sms->imei = $data[1] ?? null;
+        $sms->event_name = $data[2] ?? null;
+        $sms->session_id = $data[3] ?? null;
+        $sms->sim_ccid = $data[4] ?? null;
+        $sms->network_operator = $data[5] ?? null;
+        $sms->signal_quality = $data[6] ?? null;
+        $sms->battery_voltage = $data[7] ?? null;
+        $sms->firmware_version = $data[8] ?? null;
+        $sms->modem_fail = $data[9] ?? null;
+        $sms->network_fail = $data[10] ?? null;
+        $sms->gprs_fail = $data[11] ?? null;
 
         $sms->save();
 

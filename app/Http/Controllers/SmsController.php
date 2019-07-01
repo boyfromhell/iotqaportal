a5617803ea1fa659b6b00d30b96e1df673200549
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-07-01 14:56:23 +0530

    Auth mechanism changed

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -10,24 +10,24 @@ class SmsController extends Controller
     public function captureMessage(Request $request)
     {
         $message = $request['inboundSMSMessageNotification']['inboundSMSMessage']['message'];
-        $message = trim($message);
+        $message = $message);
         $message = str_replace(' ', '', $message);
         $data = explode(',', $message);
 
         $sms = new Sms();
         $sms->serial_number = $data[0];
-        $sms->imei = empty(trim($data[1])) ? null : $data[1];
-        $sms->event_name = empty(trim($data[2])) ? null : $data[2];
-        $sms->session_id = empty(trim($data[3])) ? null : $data[3];
-        $sms->sim_ccid = empty(trim($data[4])) ? null : $data[4];
-        $sms->network_operator = empty(trim($data[5])) ? null : $data[5];
-        $sms->signal_quality = empty(trim($data[6])) ? null : $data[6];
-        $sms->battery_voltage = empty(trim($data[7])) ? null : $data[7];
-        $sms->firmware_version = empty(trim($data[8])) ? null : $data[8];
-        $sms->modem_fail = empty(trim($data[9])) ? null : $data[9];
-        $sms->network_fail = empty(trim($data[10])) ? null : $data[10];
-        $sms->gprs_fail = empty(trim($data[11])) ? null : $data[11];
-        $sms->other = empty(trim($data[12])) ? null : $data[12];
+        $sms->imei = empty($data[1]) ? null : $data[1];
+        $sms->event_name = empty($data[2]) ? null : $data[2];
+        $sms->session_id = empty($data[3]) ? null : $data[3];
+        $sms->sim_ccid = empty($data[4]) ? null : $data[4];
+        $sms->network_operator = empty($data[5]) ? null : $data[5];
+        $sms->signal_quality = empty($data[6]) ? null : $data[6];
+        $sms->battery_voltage = empty($data[7]) ? null : $data[7];
+        $sms->firmware_version = empty($data[8]) ? null : $data[8];
+        $sms->modem_fail = empty($data[9]) ? null : $data[9];
+        $sms->network_fail = empty($data[10]) ? null : $data[10];
+        $sms->gprs_fail = empty($data[11]) ? null : $data[11];
+        $sms->other = empty($data[12]) ? null : $data[12];
 
         $sms->save();
 

45323b347b7dde53297e726e952d8ff51cdd83ef
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2019-07-01 14:51:48 +0530

    Auth mechanism changed

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -14,19 +14,18 @@ class SmsController extends Controller
 
         $sms = new Sms();
         $sms->serial_number = $data[0];
-        $sms->imei = $data[1] ?? null;
-        $sms->event_name = $data[2] ?? null;
-        $sms->session_id = $data[3] ?? null;
-        $sms->sim_ccid = $data[4] ?? null;
-        $sms->network_operator = $data[5] ?? null;
-        $sms->signal_quality = $data[6] ?? null;
-        $sms->battery_voltage = $data[7] ?? null;
-        $sms->firmware_version = $data[8] ?? null;
-        $sms->modem_fail = $data[9] ?? null;
-        $sms->network_fail = $data[10] ?? null;
-        $sms->gprs_fail = $data[11] ?? null;
-        $sms->other = $data[12] ?? null;
-
+        $sms->imei = empty(trim($data[1])) ? null : $data[1];
+        $sms->event_name = empty(trim($data[2])) ? null : $data[2];
+        $sms->session_id = empty(trim($data[3])) ? null : $data[3];
+        $sms->sim_ccid = empty(trim($data[4])) ? null : $data[4];
+        $sms->network_operator = empty(trim($data[5])) ? null : $data[5];
+        $sms->signal_quality = empty(trim($data[6])) ? null : $data[6];
+        $sms->battery_voltage = empty(trim($data[7])) ? null : $data[7];
+        $sms->firmware_version = empty(trim($data[8])) ? null : $data[8];
+        $sms->modem_fail = empty(trim($data[9])) ? null : $data[9];
+        $sms->network_fail = empty(trim($data[10])) ? null : $data[10];
+        $sms->gprs_fail = empty(trim($data[11])) ? null : $data[11];
+        $sms->other = empty(trim($data[12])) ? null : $data[12];
 
         $sms->save();
 
